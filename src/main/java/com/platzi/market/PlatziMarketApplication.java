691db9869c749865591f4758dd7457458d499fd5
jhtc5898 <johnecu15@gmail.com>
2022-02-15 21:03:11 -0500

    Uso de las application.properties

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -3,9 +3,10 @@ package com.platzi.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Presenta a spring que clase debe ejecutar
 @SpringBootApplication
 public class PlatziMarketApplication {
-
+//Este metodo ejecuta la aplicacion
 	public static void main(String[] args) {
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
